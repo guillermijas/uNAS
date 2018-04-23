c86c34435e393408f2f227cb671413b18039cc71
Guillermo Mora <guillermijas@gmail.com>
2018-04-24 00:16:18 +0200

    Add search functionality

--- puya.rb
+++ puya.rb
@@ -3,34 +3,39 @@ class Puya
   require 'nokogiri'
   require 'open-uri'
   require 'ap'
-  NAS_PATH = '/media/nas/videos'
+  NAS_PATH = '/media/nas/videos'.freeze
   # NAS_PATH = '/home/guillermo/Escritorio'.freeze
 
-  def self.articles
+  def home_articles
     articles = Nokogiri::HTML(open('http://puya.si')).css('article')
     titles = articles.map { |e| e.css('h2').text.strip }
-    links = get_links(articles)
-
+    links = home_links(articles)
     result = {}
-
     titles.zip(links) { |k, v| result[k.to_sym] = v.values.first unless v.nil? }
     result
   end
 
-  def self.get_links(articles)
-    filtered_links = []
+  def home_links(articles)
+    home_links = []
     articles.each do |art|
       links = []
       art.css('a img').map do |e|
         next unless e['src'].include? 'mega'
         links << parent_link(e)
       end
-      filtered_links << links.last
+      home_links << links.last
     end
-    filtered_links
+    home_links
   end
 
-  def self.download(puya_link, title)
+  def parent_link(img)
+    parent = img.parent
+    parent = parent.parent while parent.name != 'a' || parent.name == 'body'
+    return nil if parent.name != 'a'
+    parent
+  end
+
+  def download_link(puya_link, title)
     if puya_link.include?('puya.si')
       page = Nokogiri::HTML(open(puya_link))
       ciphertext = page.at('input[@name="crypted"]')['value']
@@ -49,14 +54,7 @@ class Puya
     end
   end
 
-  def self.parent_link(img)
-    parent = img.parent
-    parent = parent.parent while parent.name != 'a' || parent.name == 'body'
-    return nil if parent.name != 'a'
-    parent
-  end
-
-  def self.series_chapter(puya_title)
+  def article_chapter(puya_title)
     title = puya_title.delete('–')
     title.gsub!('[Final]', '')
     title.gsub!('[V2]', '')
@@ -66,9 +64,38 @@ class Puya
       chapter = 'Batch'
       title.gsub!('[Batch]', '')
     else
-      chapter = title.split[-1]
+      chapter = ''
+      chapter = title.split[-1] unless title == title.split[-1]
       title.gsub!(chapter, '')
     end
     [title, chapter]
+  end
+
+  def search(search_query)
+    page = 1
+    found_articles = []
+
+    loop do
+      link = "http://puya.si/?s=#{search_query}&paged=#{page}"
+      begin
+        articles = Nokogiri::HTML(open(link)).css('h2.entry-title')
+      rescue OpenURI::HTTPError
+        break
+      end
+      found_articles += articles.to_a
+      page += 1
+    end
+    found_articles
+  end
+
+  def open_and_get_link(puya_link)
+    article = Nokogiri::HTML(open(puya_link))
+    links = []
+    article.css('a img').map do |e|
+      next unless e['src'].include? 'mega'
+      links << parent_link(e)
+    end
+    return if links.empty?
+    links.last['href']
   end
 end
--- unas.rb
+++ unas.rb
@@ -3,6 +3,10 @@ require './puya'
 
 enable :sessions
 
+before do
+  @puya = Puya.new
+end
+
 get '/' do
   redirect to('/puya')
 end
@@ -10,16 +14,23 @@ end
 get '/puya' do
   @message = session[:msg]
   session[:msg] = nil
-  @posts = Puya.articles
+  @posts = @puya.home_articles
   haml :puya
 end
 
+get '/search' do
+  @message = session[:msg]
+  session[:msg] = nil
+  @posts = @puya.search(params[:anime])
+  haml :search
+end
+
 get '/download' do
-  session[:msg] = Puya.download(params[:link], params[:title])
-  redirect to('/puya')
+  session[:msg] = @puya.download_link(params[:link], params[:title])
+  redirect back
 end
 
 post '/download' do
-  session[:msg] = Puya.download(params[:link], 'from Mega')
-  redirect to('/puya')
+  session[:msg] = @puya.download_link(params[:link], 'from Mega')
+  redirect back
 end
